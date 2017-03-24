1d157361b5be4a01fc1172f628a363036e616b79
Celina <celgutierrez42@gmail.com>
2017-03-23 20:03:44 -0700

    refined if else statements for cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,20 +3,10 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardOne === cardTwo) {
-  alert("You found a match!");
-  } else if (cardOne === cardThree) {
-    alert("You found a match!");
-  } else if (cardOne === cardFour) {
-    alert("You found a match!");
-  } else if (cardTwo === cardThree) {
-    alert("You found a match!");
-  } else if (cardTwo === cardFour) {
-    alert("You found a match!");
-  } else if (cardThree === cardFour) {
+if ((cardOne === cardTwo) || (cardThree === cardFour)) {
   alert("You found a match!");
   } else if ((cardOne !== cardThree) && (cardOne !== cardFour) ) {
   alert("Sorry, try again.");
   } else if ((cardTwo !== cardThree) && (cardTwo !== cardFour)) {
   alert("Sorry, try again");
-} 
+}
