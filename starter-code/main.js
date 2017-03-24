7039d7568105d5d7821421b7d81a45b73dec1960
Celina <celgutierrez42@gmail.com>
2017-03-23 19:39:39 -0700

    set if else logic for card matching

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,9 +1,22 @@
-console.log("JS file is connected to HTML! Woo!")
-
 var cardOne = "queen";
-
 var cardTwo = "queen";
-
 var cardThree = "king";
-
 var cardFour = "king";
+
+if (cardOne === cardTwo) {
+  alert("You found a match!");
+  } else if (cardOne === cardThree) {
+    alert("You found a match!");
+  } else if (cardOne === cardFour) {
+    alert("You found a match!");
+  } else if (cardTwo === cardThree) {
+    alert("You found a match!");
+  } else if (cardTwo === cardFour) {
+    alert("You found a match!");
+  } else if (cardThree === cardFour) {
+  alert("You found a match!");
+  } else if ((cardOne !== cardThree) && (cardOne !== cardFour) ) {
+  alert("Sorry, try again.");
+  } else if ((cardTwo !== cardThree) && (cardTwo !== cardFour)) {
+  alert("Sorry, try again");
+} 
